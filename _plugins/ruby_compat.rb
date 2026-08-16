9fd6728ccc2cc77981859042eaa57af1816f7b25
Rickey Huang <rickeyhuang@Rickeys-MacBook-Pro.local>
2026-08-16 18:40:10 -0400

    Improve page SEO metadata and Ruby compatibility

--- _plugins/ruby_compat.rb
+++ _plugins/ruby_compat.rb
@@ -1,10 +1,19 @@
 # GitHub Pages currently pins Liquid 4.0.3, which calls String#tainted?.
 # Ruby 3.2+ removed that legacy method. Restore its final no-op behavior for
 # local builds on modern Ruby; GitHub Pages' own build environment ignores this.
-unless String.method_defined?(:tainted?)
-  class String
+unless Object.method_defined?(:tainted?)
+  class Object
     def tainted?
       false
+    end
+  end
+end
+
+# Liquid 4 also calls String#untaint from its HTML/XML escaping filters.
+unless Object.method_defined?(:untaint)
+  class Object
+    def untaint
+      self
     end
   end
 end
